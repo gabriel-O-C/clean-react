b7020d801c0cd332c5dfdde62b45bdb14c5bef4a
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-02 15:59:10 -0300

    feat: ensure RemoteAuth throws InvalidCredentialsError on 401

--- src/data/useCases/authentication/remote-auth.ts
+++ src/data/useCases/authentication/remote-auth.ts
@@ -1,4 +1,6 @@
 import { type HttpPostClient } from "@/data/protocols/http/http-post-client";
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials";
 import { type AuthenticationParams } from "@/domain/useCases/authentication";
 
 export class RemoteAuthentication {
@@ -8,6 +10,9 @@ export class RemoteAuthentication {
   ) {}
 
   async auth(params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({ url: this.url, body: params });
+    const response = await this.httpPostClient.post({ url: this.url, body: params });
+    switch(response.statusCode) {
+      case HttpStatusCode.unathorized: throw new InvalidCredentialsError();
+    }
   }
 }
