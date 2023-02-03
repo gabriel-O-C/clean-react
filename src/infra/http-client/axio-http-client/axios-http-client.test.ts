feeaadca3e2bad7b544a312661f06ca55a92e6b9
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-03 16:57:02 -0300

    test: ensure AxiosHttpClient returns correct values

--- src/infra/http-client/axio-http-client/axios-http-client.test.ts
+++ src/infra/http-client/axio-http-client/axios-http-client.test.ts
@@ -7,6 +7,13 @@ jest.mock("axios");
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const mockedAxiosResponse = {
+  data: faker.datatype.json(),
+  status: faker.random.numeric(),
+};
+
+mockedAxios.post.mockResolvedValue(mockedAxiosResponse);
+
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
@@ -22,5 +29,14 @@ describe("AxiosHttpClient", () => {
     const sut = makeSut();
     await sut.post(request);
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+  });
+
+  it("Should return the correct StatusCode and body", async () => {
+    const sut = makeSut();
+    const response = await sut.post(mockPostRequest());
+    expect(response).toEqual({
+      statusCode: mockedAxiosResponse.status,
+      body: mockedAxiosResponse.data,
+    });
   });
 });
