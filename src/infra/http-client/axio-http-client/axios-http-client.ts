6027020d8aea4a37a1a5d4e38f8138b3196b406e
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-03 16:56:46 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http-client/axio-http-client/axios-http-client.ts
+++ src/infra/http-client/axio-http-client/axios-http-client.ts
@@ -1,8 +1,16 @@
-import { type HttpPostParams } from "@/data/protocols/http";
+import {
+  type HttpResponse,
+  type HttpPostParams,
+  type HttpPostClient,
+} from "@/data/protocols/http";
 import axios from "axios";
 
-export class AxiosHttpClient {
-  async post(params: HttpPostParams<unknown>): Promise<void> {
-    await axios.post(params.url, params.body);
+export class AxiosHttpClient implements HttpPostClient<unknown, unknown> {
+  async post(params: HttpPostParams<unknown>): Promise<HttpResponse<unknown>> {
+    const response = await axios.post(params.url, params.body);
+    return {
+      statusCode: response.status,
+      body: response.data,
+    };
   }
 }
