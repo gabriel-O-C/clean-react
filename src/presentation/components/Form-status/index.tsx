1db8f82481356cd9609ec8eef6bf1371ceafb439
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-08 09:48:45 -0300

    feat: ensure login will not load spinner and error on start

--- src/presentation/components/Form-status/index.tsx
+++ src/presentation/components/Form-status/index.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Spinner } from "../Spinner";
-
+import Context from "@/presentation/contexts/form/form-context";
 export const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context);
   return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      <div className="top-0 left-0 flex h-full w-full items-center justify-center">
-        <Spinner />
-      </div>
-      <span className="text-primaryLight">Erro</span>
+    <div
+      data-testid="error-wrap"
+      className="flex flex-col items-center justify-center gap-8"
+    >
+      {isLoading ? (
+        <div className="top-0 left-0 flex h-full w-full items-center justify-center">
+          <Spinner />
+        </div>
+      ) : null}
+
+      {errorMessage ? <span className="text-primaryLight">{errorMessage}</span> : null }
     </div>
   );
 };
--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Footer,
   FormStatus,
@@ -6,28 +6,42 @@ import {
   LoginHeader,
 } from "@/presentation/components";
 
+import Context from "@/presentation/contexts/form/form-context";
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
+
 export const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ""
+  });
   return (
     <div className="flex h-screen w-full flex-col justify-between">
       <LoginHeader />
-      <form className="flex w-[400px] flex-col justify-center gap-4 self-center rounded-lg bg-white p-8 shadow-md">
-        <h2 className="text-center text-[16px] font-bold uppercase text-primaryDark">
-          Login
-        </h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
+      <Context.Provider value={state}>
+        <form className="flex w-[400px] flex-col justify-center gap-4 self-center rounded-lg bg-white p-8 shadow-md">
+          <h2 className="text-center text-[16px] font-bold uppercase text-primaryDark">
+            Login
+          </h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
 
-        <button
-          type="submit"
-          className="h-8 rounded-lg border-[1px] border-primaryLight bg-primary px-8 text-[16px] text-white hover:opacity-90"
-        >
-          Entrar
-        </button>
-        <span className="cursor-pointer text-center lowercase text-primary hover:underline">
-          Criar conta
-        </span>
-        <FormStatus />
-      </form>
+          <button
+            type="submit"
+            className="h-8 rounded-lg border-[1px] border-primaryLight bg-primary px-8 text-[16px] text-white hover:opacity-90"
+          >
+            Entrar
+          </button>
+          <span className="cursor-pointer text-center lowercase text-primary hover:underline">
+            Criar conta
+          </span>
+          <FormStatus />
+        </form>
+
+      </Context.Provider>
       <Footer />
     </div>
   );
