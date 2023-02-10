d25c022bc7482a468e7c34452b255fc5adad1d0d
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-10 14:35:50 -0300

    test: ensure login present error if email is invalid

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -26,9 +26,8 @@ export const Login: React.FC<Props> = ({ validation }: Props) => {
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate("email", state.email)
-    })
-    
+      emailError: validation.validate("email", state.email),
+    });
   }, [state.email]);
 
   useEffect(() => {
