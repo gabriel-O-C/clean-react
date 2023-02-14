c1dade365e43316593d7cd7dc515eb5597282791
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-14 16:00:58 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/Login/login.test.tsx
+++ src/presentation/pages/Login/login.test.tsx
@@ -85,4 +85,18 @@ describe("Login component", () => {
     expect(passwordStatus.title).toBe("Tudo certo!");
     expect(passwordStatus.textContent).toBe("🟢");
   });
+
+  it("Should show valid email state if Validation succeeds", () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const emailInput = sut.getByTestId("email");
+    const emailStatus = sut.getByTestId("email-status");
+
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() },
+    });
+
+    expect(emailStatus.title).toBe("Tudo certo!");
+    expect(emailStatus.textContent).toBe("🟢");
+  });
 });
